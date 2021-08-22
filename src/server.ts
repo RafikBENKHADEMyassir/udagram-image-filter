397300607489248d040c2ba30e1b54dabf395653
rafikbenkhadem <rafik.benkhadem@gmail.com>
2021-08-22 10:06:30 +0100

    Fixing variables typscript typing, and addin screenshots

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {Request, Response} from 'express';
 
 (async () => {
 
@@ -28,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage/",async ( req, res)=>{
+  app.get("/filteredimage/",async (req: Request,res: Response)=>{
     let {image_url}: any = req.query;
     if( !image_url ) {
       return res.status(422)
@@ -48,7 +49,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async (req: Request,res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
